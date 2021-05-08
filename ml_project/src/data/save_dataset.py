64ae9cf03719f8691b1ffb39c3121af93e6537ef
SergeyNefedov <sergeynefedov832@gmail.com>
2021-05-08 15:51:35 +0300

    fix typing

--- ml_project/src/data/save_dataset.py
+++ ml_project/src/data/save_dataset.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import numpy as np
 
 
 def save_data(df: pd.DataFrame, output_path: str):
     df.to_csv(output_path, index=False)
 
 
-def target_to_dataframe(predicts):
+def target_to_dataframe(predicts: np.ndarray) -> pd.DataFrame:
     """
 
 
